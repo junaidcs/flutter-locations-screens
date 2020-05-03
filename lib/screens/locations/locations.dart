717c88e518b72eb2c0720dcc6e133378b4dc2d53
Muhammad Junaid <m.junaidcs@gmail.com>
2020-05-04 00:29:47 +0500

    locations screen

---
+++ lib/screens/locations/locations.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+// models
+import '../../models/location.dart';
+
+// screens
+import '../../main.dart';
+// import '../../main.dart';
+import './single-location.dart';
+
+class Locations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final List<Location> locations = Location.fetchAll();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Locations'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ...locations
+              .map(
+                (location) => GestureDetector(
+                  onTap: () => _onTabLocation(context, location.id),
+                  child: SingleLocation(location.title, location.imagePath),
+                ),
+              )
+              .toList()
+        ],
+      ),
+    );
+  }
+
+  static _onTabLocation(BuildContext context, locationID) {
+    print(locationID);
+    Navigator.pushNamed(context, LocationDetailsRoute,
+        arguments: {'id': locationID});
+  }
+}
